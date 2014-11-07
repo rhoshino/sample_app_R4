f9697e77968ca4e048cc11289e3ee12e55b83e34
rhoshino <rhoshino@unisrv.jp>
2014-11-07 17:16:07 +0900

    Fenish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require 'spec_helper'
+
+describe "UserPages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before {visit signup_path}
+
+    it {is_expected.to have_content('Sign up')}
+    it {is_expected.to have_title(full_title('Sign up'))}
+
+  end
+
+end
