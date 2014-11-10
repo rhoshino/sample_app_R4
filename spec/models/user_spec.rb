82c5e4e2731589ffa65c7557926ad03ae785cf60
rhoshino <rhoshino@unisrv.jp>
2014-11-10 17:54:24 +0900

    Fenished exercise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,7 +44,8 @@ describe User do
                     user_at_foo.org
                     example.user@foo.
                     foo@bar_baz.com
-                    foo@bar+baz.com]
+                    foo@bar+baz.com
+                    foo@bar..com]
       addresses.each do |invalid_address|
 
         @user.email = invalid_address
@@ -76,6 +77,16 @@ describe User do
     end
 
     it{ is_expected.not_to be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email){"Foo@ExAMPle.CoM"}
+
+    it "should be saved as all lower-case " do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 
   describe "when password is not present" do
