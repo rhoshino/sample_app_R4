2b45204f160014baa7d38b4714d82fe0270b8cbc
rhoshino <rhoshino@unisrv.jp>
2014-11-11 15:17:10 +0900

    Fenish exersise

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,6 +31,14 @@ describe "User pages" do
       it "shoud not create a user" do
         expect{click_button submit}.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before {click_button submit}
+
+        it{ is_expected.to have_title('Sign up')}
+        it{ is_expected.to have_content('error')}
+      end
+
     end
 
     describe "with valid information" do
@@ -44,6 +52,16 @@ describe "User pages" do
       it "should create a user" do
         expect{click_button submit}.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit}
+        let(:user){User.find_by(email: 'user@example.com')}
+
+        it {is_expected.to have_title(user.name)}
+        it {is_expected.to have_selector('div.alert.alert-success', text:'Welcome')}
+
+      end
+
     end
   end
 
